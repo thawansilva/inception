bef8123a8f2be46daa8b3dfea63dc490fd391b50
thaperei <thaperei@thaperei.thaperei.42.fr>
2026-06-27 17:27:37 -0300

    build: Defining mariadb configs and adjust env variables to be used in script

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password.txt)
-DB_PASSWORD=$(cat /run/secrets/db_password.txt)
-DB_USER=$(cat /run/secrets/db_user.txt)
+envsubst '$DB_PORT,$DB_USER,$DB_DATADIR' < /etc/mariadb-server.cnf.template > /etc/my.cnf.d/mariadb-server.cnf
 
-DATADIR=/var/lib/mysql
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+DB_PASSWORD=$(cat /run/secrets/db_password)
 
 if [ ! -d "$DATADIR/mysql" ]; then
 	echo "Initializing MariaDB configuration"
 
-	mariadb-install-db --user=mysql --datadir="$DATADIR"
+	mariadb-install-db --user=$DB_USER --datadir="$DB_DATADIR"
 	TMP_FILE="/tmp/init_db.sql"
 
 	cat << EOF > $TMP_FILE
@@ -21,13 +20,13 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 CREATE USER IF NOT EXISTS 'root'@'127.0.0.1' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 GRANT ALL PRIVILEGES ON  *.* TO 'root'@'127.0.0.1' WITH GRANT OPTION;
 
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-	mariadbd --user=mysql --datadir="$DATADIR" --bootstrap < $TMP_FILE
+	mariadbd --user=$DB_USER --datadir="$DB_DATADIR" --bootstrap < $TMP_FILE
 	rm -f $TMP_FILE
 
 	echo "MariaDB configuration completed"
@@ -35,4 +34,4 @@ else
 	echo "Mariadb storage already initialized"
 fi
 
-exec mariadbd --user=mysql --datadir="$DATADIR" --console
+exec mariadbd --defaults-file="/etc/my.cnf.d/mariadb-server.cnf" --console
