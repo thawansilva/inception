19bab824acb5c68d050b90a1798c5c71e4e0ae1e
Thawan Silva <twnpsilva@gmail.com>
2026-06-30 22:43:20 -0300

    fix: Change parameters config and add health checker before config mariadb

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -2,36 +2,48 @@
 
 set -e
 
-envsubst '$DB_PORT,$DB_USER,$DB_DATADIR' < /etc/mariadb-server.cnf.template > /etc/my.cnf.d/mariadb-server.cnf
+envsubst '$DB_PORT' < /etc/mariadb-server.cnf.template > /etc/my.cnf.d/mariadb-server.cnf
 
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 DB_PASSWORD=$(cat /run/secrets/db_password)
 
+DB_DATADIR=/var/lib/mysql
+
 if [ ! -d "$DB_DATADIR/mysql" ]; then
 	echo "Initializing MariaDB configuration"
 
-	mariadb-install-db --user=mysql --datadir="$DB_DATADIR"
-	TMP_FILE="/tmp/init_db.sql"
+	mariadb-install-db --user=mysql --datadir="$DB_DATADIR" > /dev/null
+fi
 
-	cat << EOF > $TMP_FILE
+echo "Starting temporary MARIADB Instance"
+mysqld --skip-networking --user=mysql --socket=/run/mysqld/mysqld.sock & pid="$!"
+
+echo "Waiting for MariaDB to be ready..."
+until mysqladmin --socket=/run/mysqld/mysqld.sock ping >/dev/null 2>&1; do
+	sleep 1
+done
+echo "MariaDB is ready"
+
+echo "Setting up MariaDB"
+
+echo senha $DB_PASSWORD
+
+mysql --socket=/run/mysqld/mysqld.sock -u root << EOF
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
-CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
-GRANT ALL PRIVILEGES ON  *.* TO 'root'@'localhost' WITH GRANT OPTION;
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 
-CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-	mariadbd --user=mysql --datadir="$DB_DATADIR" --bootstrap < $TMP_FILE
-	rm -f $TMP_FILE
+echo "Shutting down temporary MariaDB..."
+mysqladmin --socket=/run/mysqld/mysqld.sock -u root -p"${DB_ROOT_PASSWORD}" shutdown
 
-	echo "MariaDB configuration completed"
-else
-	echo "Mariadb storage already initialized"
-fi
+# Wait for shutdown
+wait "$pid" || true
 
-exec mariadbd --defaults-file="/etc/my.cnf.d/mariadb-server.cnf" --console
+echo "MariaDB configuration completed. Starting MariaDB..."
+exec mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock
