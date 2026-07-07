add434ea1c18c6783df55ce22e05141ca54010dd
Thawan Silva <twnpsilva@gmail.com>
2026-07-06 22:24:48 -0300

    wip: change ssl cert folder, finish TLS certificate generation, init nginx configuration

--- srcs/requirements/nginx/tool/setup_nginx.sh
+++ srcs/requirements/nginx/tool/setup_nginx.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
 set -e
 
+envsubst '$WEB_PORT $DOMAIN_NAME $WP_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+: "${DOMAIN_NAME:=localhost}"
+
 if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
 	echo "Generating self-signed SSL certificate for $DOMAIN_NAME"
+	openssl req -x509 \
+		-newkey rsa:4096 \
+		-keyout /etc/nginx/ssl/nginx.key \
+		-out /etc/nginx/ssl/nginx.crt \
+		-sha256 \
+		-days 365 \
+		-nodes \
+		-subj "/C=BR/ST=SaoPaulo/L=SaoPaulo/O=42SaoPaulo/CN=$DOMAIN_NAME"
+
+	chmod 600 /etc/nginx/ssl/nginx.key
+	chmod 644 /etc/nginx/ssl/nginx.crt
+	echo "SSL certificate generated at /etc/nginx/ssl"
 else 
 	echo "SSL certificate already exists."
 fi
 
+echo "Testing Nginx configuration..."
 nginx -t
 echo "Nginx configuration test passed"
+
+echo "Starting Nginx..."
