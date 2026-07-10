f829ee78ae38fd5d25812e3adae00dfa92f2b8a3
Thawan Silva <twnpsilva@gmail.com>
2026-07-10 20:37:05 -0300

    feat(ftp): finish config of ftp server

--- srcs/requirements/bonus/ftp/tools/setup_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/setup_ftp.sh
@@ -4,8 +4,9 @@ set -e
 
 FTP_PASSWORD=$(cat /run/secrets/ftp_password)
 
-envsubst '$FTP_USER $FTP_PORT $FTP_PASV_PORT_MIN $FTP_PASV_PORT_MAX' \
-	< /etc/vsftpd/vsftpd.conf.template > /etc/vsftpd/vsftpd.conf
+mkdir -p /var/run/vsftpd/empty
+
+envsubst '$FTP_PORT $FTP_PASV_MIN_PORT $FTP_PASV_MAX_PORT' < /etc/vsftpd/vsftpd.conf.template > /etc/vsftpd/vsftpd.conf
 
 if ! id "$FTP_USER" > /dev/null 2>&1; then
 	echo "Adding user '$FTP_USER'..."
